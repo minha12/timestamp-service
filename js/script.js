437e96d20ae3d4a3b323d1292ee6f7ce776c7db0
minha12 <34024199+minha12@users.noreply.github.com>
2025-01-07 17:52:21 +0000

    Deploying to gh-pages from @ minha12/timestamp-sevice@147b9d24ae6984c2331fcec4d88ad62b07bd5973 🚀

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Configure API base URL
+const BASE_URL = window.location.hostname === 'localhost' 
+? '' 
+: 'https://minha12.github.io/timestamp-service';
+
+
+async function testAPI() {
+  const dateInput = document.getElementById('dateInput').value;
+  const result = document.getElementById('result');
+  
+  try {
+    const response = await fetch(`${BASE_URL}/api/timestamp/${dateInput}`);
+    const data = await response.json();
+    result.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+  } catch (error) {
+    result.innerHTML = `Error: ${error.message}`;
+  }
+}
+
+async function testCurrentTime() {
+  const result = document.getElementById('result');
+  
+  try {
+    const response = await fetch(`${BASE_URL}/api/timestamp`);
+    const data = await response.json();
+    result.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+  } catch (error) {
+    result.innerHTML = `Error: ${error.message}`;
+  }
+}
